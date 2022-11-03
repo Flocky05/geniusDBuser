dc4b869782f30c05f33747b0565e4b2de267fec4
Flocky05 <md.rakibul.islam.0501@gmail.com>
2022-11-04 02:03:01 +0600

    update index

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const cors = require('cors');
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 require('dotenv').config()
 
 const app = express();
@@ -14,14 +14,30 @@ app.use(express.json());
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.cklgizg.mongodb.net/?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
-client.connect(err => {
-    const collection = client.db("test").collection("devices");
-    // perform actions on the collection object
-    client.close();
-});
-console.log(uri);
 
+async function run() {
+    try {
+        const serviceCollection = client.db("geniusCar").collection('services');
+        app.get('/services', async (req, res) => {
+            const quary = {}
+            const cursor = serviceCollection.find(quary);
+            const services = await cursor.toArray();
+            res.send(services);
 
+        });
+
+        app.get('/service/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: ObjectId(id) };
+            const service = await serviceCollection.findOne(query);
+            res.send(service);
+        })
+    } finally {
+
+    }
+
+}
+run().catch(error => console.log(error));
 
 app.get('/', (req, res) => {
     res.send('Genious car server is running now');
